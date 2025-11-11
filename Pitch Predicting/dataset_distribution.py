0d0eafb71c9943a050b89721e5cbf900b65e03f1
Christian Rocha <christianrocha204@gmail.com>
2025-11-10 19:10:23 -0500

    .

--- Pitch Predicting/dataset_distribution.py
+++ Pitch Predicting/dataset_distribution.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pybaseball import statcast
 
 # load your same dataset
-df = statcast(start_dt='2021-04-01', end_dt='2024-09-30')  # or whatever range you used
+df = statcast(start_dt='2025-09-01', end_dt='2025-09-30')  # or whatever range you used
 
 # count pitch types
 pitch_counts = df['pitch_type'].value_counts(dropna=False)
