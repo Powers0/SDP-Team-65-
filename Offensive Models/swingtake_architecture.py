11a68886a90b12d69cdb66e8817f20718836aa1a
Ryan Powers <ryan.p.powers@uconn.edu>
2026-02-06 13:46:08 -0500

    first offensive model commit

---
+++ Offensive Models/swingtake_architecture.py
@@ -0,0 +1,23 @@
+from tensorflow.keras.layers import Input, Dense, Dropout, Concatenate
+from tensorflow.keras.models import Model
+
+def build_swingtake_model(pitch_type_dim: int, extra_dim: int):
+    """
+    Inputs:
+      - pitchtype_input: (pitch_type_dim,)  e.g., 7-dim softmax
+      - extra_input:     (extra_dim,)       e.g., predicted x,z, dist, strike_prob
+
+    Output:
+      - sigmoid: P(swing)
+    """
+    pitchtype_input = Input(shape=(pitch_type_dim,), name="pitchtype_probs")
+    extra_input = Input(shape=(extra_dim,), name="extra_features")
+
+    x = Concatenate()([pitchtype_input, extra_input])
+    x = Dense(64, activation="relu")(x)
+    x = Dropout(0.30)(x)
+    x = Dense(32, activation="relu")(x)
+    x = Dropout(0.20)(x)
+    out = Dense(1, activation="sigmoid")(x)
+
+    return Model(inputs=[pitchtype_input, extra_input], outputs=out)
